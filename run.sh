2c6b0f6c325775c222fdfa1325580efd48381c4f
Toke von Ryberg <tokeryberg@gmail.com>
2015-11-23 17:02:17 +0200

    Remove dot from zshrc and vimrc in run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
 # input fonts
+#http://input.fontbureau.com/build/?fontSelection=whole&a=0&g=0&i=0&l=0&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email=
 
-http://input.fontbureau.com/build/?fontSelection=whole&a=0&g=0&i=0&l=0&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email=
+#install brew and cegs
 
+#install and setup node
+./node.sh
 
 #link zshrc
-
-ln -s "$HOME/Private/dotfiles/.zshrc" "$HOME/.zshrc"
-ln -s "$HOME/Private/dotfiles/.vimrc" "$HOME/.vimrc"
-
-# use node in every new shell
-nvm alias default node
+ln -s "$PWD/zshrc" "$HOME/.zshrc"
+ln -s "$PWD/vimrc" "$HOME/.vimrc"
